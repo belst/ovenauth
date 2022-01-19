6c9a46bf3782d2d149c9a17a8c27d9a8a8493b70
belst <git@bel.st>
2022-01-19 01:49:20 +0100

    Change Homepage

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -6,22 +6,27 @@ import { AuthService } from "./store/AuthService";
 
 const Home: Component = () => {
     const authService = useService(AuthService);
+    const endpoint = import.meta.env.VITE_PROTOCOL + import.meta.env.VITE_BASEURL;
 
     return <>
         <Layout>
-            <For each={authService().users}>
-                {(user) =>
-                    <div class="card lg:card-side card-bordered">
-                        <div class="card-body">
-                            <h2 class="card-title">{user.username}</h2>
-                            <p>A very cool Fluss</p>
-                            <div class="card-actions">
-                                <Link href={`/${user.username}`} class="btn btn-primary">Watch</Link>
+            <div class="flex space-x-4">
+                <For each={authService().users}>
+                    {(user) =>
+                        <div class="basis-1/4 aspect-video card shadow-xl card-bordered image-full">
+                            <figure class="aspect-video">
+                                <img src={`${endpoint}/thumbs/${user.username}/thumb.png`} />
+                            </figure>
+                            <div class="justify-end card-body">
+                                <h2 class="card-title">{user.username}</h2>
+                                <div class="card-actions">
+                                    <Link href={`/${user.username}`} class="btn btn-primary">Watch</Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                }
-            </For>
+                    }
+                </For>
+            </div>
         </Layout>
     </>;
 };
