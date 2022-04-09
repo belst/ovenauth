22dd03b1f8c06066353c3c2ffcedb5f40263ee84
belst <git@bel.st>
2022-04-10 01:03:23 +0200

    cleanup

--- frontend/src/Stream.tsx
+++ frontend/src/Stream.tsx
@@ -1,42 +1,28 @@
 import { useParams } from "solid-app-router";
-import { Component, createEffect, onCleanup, onMount } from "solid-js";
-import OvenPlayer from 'ovenplayer';
-import { useRegistry } from "solid-services";
+import { Component } from "solid-js";
 import Title from "./Title";
+import Player from "./Player";
 
 const Stream: Component = () => {
     const params = useParams();
-    let ref;
 
     const endpoint = import.meta.env.VITE_BASEURL;
 
-    let player;
-
-    onMount(() => {
-        player = OvenPlayer.create(ref, {
-            sources: [
-                {
-                    type: 'webrtc',
-                    file: `wss://${endpoint}/ws/${params.user}`,
-                }
-            ]
-        });
-        player.play();
-    });
-
-    onCleanup(() => {
-        player?.remove();
-    });
+    const css = {
+        'aspect-ration': '16 / 9',
+        margin: '0 auto',
+        'max-width': '100%',
+        'max-height': 'calc(100vh - 48px - 16px - 16px)',
+    };
 
     return (
         <>
             <Title value={params.user} />
-            <div style="margin: 0 auto; height: calc(100vh - 48px - 16px - 16px); width: calc((100vh - 48px - 16px - 16px) * 1.77777777778)">
-                <div id="player" ref={ref}></div>
+            <div style={css}>
+                <Player url={`wss://${endpoint}/ws/${params.user}`} instance={params.user} autoplay={true} id="player"></Player>
             </div>
         </>
     );
 };
 
 export default Stream;
-
