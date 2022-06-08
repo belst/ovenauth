3e110e3803bc1ee8d62278e4d52be601ed6c8cbb
Philipp <philipp@f1ndus.de>
2022-06-08 15:31:18 +0200

    This Commit introduces basic access control to streams and adds example infrastructure code for a dockerized setup
    This Patch adds basic stream access management to ovenauth.
    User can choose who can watch the stream:
    - Per default only users that are logged in can watch anything
    - A streamer can further restrict access to a subset of registered accounts within the dashboard
    - A streamer can also flag their stream as a public stream, if the flag is set also unregistered visitor are able to watch it.
    
    After login the frontend sends a token generate request to the backend. The backend generates a uuid, saves it to the
    database and sends it back to the frontend.
    With this token the logged-in user is able to access restricted resources on the server.
    
    Nginx or a server that supports auth requests is required for this feature to work.
    Frontend:
    
        AuthService:
        - When logged out AuthService now has guest_user with guest token, instead of just being null
        - Implemented refresh token logic to generate new token
        - Implemented allowedUsers() and allowedToWatch() backend call
    
        Dashboard:
        - Now scrollable on small mobile devices
    
        Navbar:
        - Made Navbar usable on mobile devices
    
        api:
        - Added username and token arguments to makeRequest so that it is possible to send both as request headers
    
        Footer:
        - used environment variable for text string
    
        index.css:
        - included loading spinner
    
        package-json:
        - Bumped solid-app-router to 0.3.3, fixes https://github.com/solidjs/solid-router/issues/112
    
    Backend:
    
        - implemented Token CRUD Logic
    
    Infra:
        - Included basic docker compose environment to set up nginx/ove/ovenauth
        - Included auth requests to nginx.conf

--- frontend/src/types/user.interface.ts
+++ frontend/src/types/user.interface.ts
@@ -2,6 +2,12 @@ export interface IUser {
     id: number;
     username: string;
     hidden: boolean;
+    public: boolean;
+}
+
+export interface UserPermission {
+    user: IUser,
+    permitted: boolean
 }
 
 export interface IStreamOption {
